258ff4732dc0742dcc7bae8079c0c38a784f1f25
Doughmination <clovetwilight3@outlook.com>
2026-03-01 10:31:02 +0000

    Delete dist directory

--- dist/api/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/** src/api/types/index.ts
- * @license MIT
- * Copyright (c) 2025 Clove Twilight
- * See LICENSE file in the root directory for full license text.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
